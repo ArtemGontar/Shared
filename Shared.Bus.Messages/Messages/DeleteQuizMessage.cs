dccd1b1f28c5441f3ae2b43cb1f4c7c8d13ba28a
art.hontar <art.hontar@andersenlab.com>
2020-05-05 23:58:24 +0300

    add Events

---
+++ Shared.Bus.Messages/Messages/DeleteQuizMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Bus.Messages.Messages
+{
+    public class DeleteQuizMessage : IMessage
+    {
+        public Guid OwnerId { get; set; }
+        public Guid QuizId { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+    }
+}
--- Shared.Bus.Messages/ReceiveEndpointExtensions.cs
+++ Shared.Bus.Messages/ReceiveEndpointExtensions.cs
@@ -14,5 +14,16 @@ namespace Shared.Bus.Messages
 
             return receiveEndpointAttr.Uri;
         }
+
+        public static Uri GetReceiveEndpoint(this Type type)
+        {
+            var customAttributes = type.GetCustomAttributes(typeof(ReceiveEndpointAttribute), false);
+            if (customAttributes == null || customAttributes.Length == 0)
+                throw new ArgumentNullException(nameof(customAttributes));
+
+            var receiveEndpointAttr = (ReceiveEndpointAttribute)customAttributes[0];
+
+            return receiveEndpointAttr.Uri;
+        }
     }
 }
