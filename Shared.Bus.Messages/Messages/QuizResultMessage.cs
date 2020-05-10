3f5be9ed7624e174ee9998bdac55621a539ac11a
art.hontar <art.hontar@andersenlab.com>
2020-05-10 15:42:54 +0300

    add roles and add message

--- Shared.Bus.Messages/Messages/QuizResultMessage.cs
+++ Shared.Bus.Messages/Messages/QuizResultMessage.cs
@@ -9,6 +9,8 @@ namespace Shared.Bus.Messages
         public int CorrectAnswersCount { get; set; }
         public int WrongAnswersCount { get; set; }
         public double CorrectPercent { get; set; }
+        public Guid QuizId { get; set; }
+        public Guid UserId { get; set; }
         public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
     }
 }
